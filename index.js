3d3e78a3e2f76b2c154960e7c494b45c96e4e696
sravittxof <travis.fox90@gmail.come>
2021-04-02 18:05:43 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+document.querySelector('#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+const name = 'Travis'
+
+newHeader.innerHTML = `${name} is the champion`
+
+document.body.appendChild(newHeader);
